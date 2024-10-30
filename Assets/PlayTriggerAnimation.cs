4f68ff50cfa2c955e1345bbd0feb115e920c2b3d
codefrofun <blackpinkaricat@gmail.com>
2024-10-29 21:49:08 -0300

    Added camera, scripts for trigger function, and trigger button.

---
+++ Assets/PlayTriggerAnimation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject AnimatedObject;
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = AnimatedObject.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        _animator.enabled = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        _animator.enabled = false;
+    }
+}
